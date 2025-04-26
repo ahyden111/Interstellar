4af9358c536e6897a3238b15da03a3b4dd899540
ahyden111 <ahyden@friendshipchristian.net>
2025-04-26 15:59:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
+  challenge: true, // Set to true if you want to enable password protection.
+  users: {  // You can add multiple users by doing username: 'password'.
     interstellar: "password",
-  },
-};
-
-export default config;
+git clone https://github.com/UseInterstellar/Interstellar
+cd Interstellar},
+git clone --branch Ad-Free https://github.com/UseInterstellar/Interstellar
+cd Interstellar};
+pnpm i
+pnpm start
+  export default config;config=true pnpm start
